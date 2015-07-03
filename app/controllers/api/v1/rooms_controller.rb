4ccdd90aa3dbfcbc67855c64a73790cfe369b682
prakash <prakashpoudel@lftechnology.com>
2015-07-04 00:43:08 +0545

    rooms controller api complete

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,13 +1,44 @@
-class Api::V1::ScoreController < ApplicationController
-def create
-  room = Message.create params_model
-  json  = model.to_json
-  channel = "private-conversation.#{params[:user_id]}"
-  Pusher[channel].trigger 'messages/create', json
-end
+class Api::V1::RoomsController < ApplicationController
 
-private
-def params_model
-  params.require(:message).permit(:score)
-end
+  # @url /api/v1/rooms/create
+  # @action POST
+  #
+  # creates new room
+  #
+  # @example_response
+  # ```
+  # {"success": true,
+  # "message": "room created successfully"
+  # }
+  # ```
+  def create
+    room = Room.new room_params
+    if room.save!
+      render json: {success: true, message: t(:room_created_successfully)}, status: 200
+    else
+      render json: {success: false, error: t(:room_could_not_be_created)}, status: 400
+    end
+  end
+
+  # @url /api/v1/rooms
+  # @action Get
+  #
+  # lists all rooms
+  #
+  # @example_response
+  # ```
+  # [{"id":1,"name":"lobby","created_at":"2015-07-03T18:21:01.000Z","updated_at":"2015-07-03T18:21:01.000Z"}]
+  # ```
+  def index
+    @rooms = Room.all
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
+  end
+
+  def params_model
+    params.require(:message).permit(:score)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
+      resources :rooms
       resources :messages
       resources :users do
         collection do
