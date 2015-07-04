3b9e9c2c629b0793b30964a0980df07cf4289770
showingWarf91 <joshi.anujo7@gmail.com>
2015-07-04 12:03:26 +0545

    LFPP issue resolved

--- spec/controllers/api/v1/rooms_controller_spec.rb
+++ spec/controllers/api/v1/rooms_controller_spec.rb
@@ -5,22 +5,11 @@ RSpec.describe Api::V1::RoomsController, type: :controller do
   let(:json_response) { JSON.parse(response.body) }
   let(:room) {FactoryGirl.create(:room)}
   let(:user) {FactoryGirl.create(:user, room_id: room.id)}
-  let(:@user) do
-    stub_model User, id: 5, name: "Katherine", provider: 'Kat', uid: '22222222222', email: 'hello@gmail.com', role: 0, room_id: 1
-  end
-  before(:each) do
-    @user = user
-  end
 
-  describe '#index' do
-    before do
-      get :index
-    end
-  end
   describe '#create' do
     context 'with valid params' do
       before do
-        post :create, room:{name: 'newRoom'}
+        post :create, room:{name: 'newRoom'}, project_name: 'LFPLAN'
       end
       it { expect(response).to have_http_status(200) }
     end
