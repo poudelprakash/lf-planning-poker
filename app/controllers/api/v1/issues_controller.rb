395093405cbe9461fb64b728adb0d079637fdb5b
gurungsuman <gurung.suman15@gmail.com>
2015-07-04 06:54:27 +0545

    LFPP: simple authentication implemented

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::IssuesController < ApplicationController
   def index
     project_name = params[:projectName]
-    binding.pry
     response = HTTParty.get("http://lf-planningpoker.atlassian.net/rest/api/2/search?project="+project_name, headers:{
                                                                                                                "Authorization" => 'Basic YWRtaW46bGVhcGZyb2c=\n'
                                                                                                            }
