f33c79c439001f17886486b0aca0fa4ecc37a8de
prakash <prakashpoudel@lftechnology.com>
2015-07-04 06:00:14 +0545

    LFPP add stories to project

---
+++ app/controllers/api/v1/stories_controller.rb
@@ -0,0 +1,10 @@
+class StoriesController < ApplicationController
+  
+  def assign_point
+    story = Story.find(params[:id])
+    story_point = params[:story_point]
+    story.update(story_point: story_point)
+    render json: {success: true, message: t(:story_point_updated)}, status: 200
+  end
+
+end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ActiveRecord::Base
   has_many :users
+  has_many :stories
   validates :name, presence: :true
 end
